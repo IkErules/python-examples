ef0da681c20a8a345a4418e841af5d0b8c70d54e
Florian Thalmann <Florian.Thlamann@leuchterag.ch>
2020-05-15 10:06:34 +0200

    Further refactoring filter-map-reduce example

--- excercises/filter_map_reduce.py
+++ excercises/filter_map_reduce.py
@@ -1,13 +1,9 @@
+from functools import reduce
+
 """
 Übung Java 8, Teil 1+2
 ---- Nr. 4 ----
 """
-
-from functools import reduce
-
-
-def map_function_to_upper(x: str):
-    return x.upper()
 
 
 def filter_map_reduce(names):
@@ -16,18 +12,26 @@ def filter_map_reduce(names):
     return reduce(lambda x, y: x + " " + y, names)
 
 
-def start():
-    names = ["Susanna", "Joe", "Lu", "Timmy", "Rafael", "Lisa"]
-    result = filter_map_reduce(names)
-    result_chained = filter_map_reduce_chained(names)
-    print(result)
-    print(result_chained)
+def map_function_to_upper(x: str):
+    return x.upper()
 
 
+"""
+Sehr unschön und nicht zu gebrauchen! Um eine chained-Version zu verwenden, wird filter-map-reduce
+zu reduce-map-filter.
+"""
 def filter_map_reduce_chained(names):
     return reduce(lambda x, y: x + " " + y,
                   map(lambda x: x.upper(),
                       filter(lambda x: 3 <= len(x) <= 4, names)))
+
+
+def start():
+    names = ["Susanna", "Joe", "Lu", "Timmy", "Rafael", "Lisa"]
+    result = filter_map_reduce(names)
+    result_chained = filter_map_reduce_chained(names)
+    print(f"result: {result}")
+    print(f"result chained: {result_chained}")
 
 
 start()
